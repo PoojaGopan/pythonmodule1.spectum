fd33d4ec0b9d2c9d95182f172dd486a62dbec79b
POOJA GOPAN <153507248+PoojaGopan@users.noreply.github.com>
2024-01-03 12:59:53 +0530

    Add files via upload

---
+++ FAQ8.py
@@ -0,0 +1,25 @@
+#zero division error
+# def compute_division():
+#     try:
+#         result = 5 / 0
+#         return result
+#     except ZeroDivisionError:#exception type
+#         print("Error: Division by zero is not allowed.")
+#         return None
+
+# # Call the function
+# result = compute_division()
+
+# # Check if the result is None (indicating an error)
+# if result is not None:
+#     print("Result:", result)
+
+
+#The try and except blocks in Python are used for error handling
+#try:
+    # Code that might raise an exception
+    # ...
+#except ExceptionType as e:
+    # Code to handle the exception
+#If no exception occurs, the code inside the except block is skipped
+#Using try and except allows you to gracefully handle errors and prevent your program from crashing when unexpected issues arise
